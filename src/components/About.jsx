b2a9b8d9fe83f58e7598e774a452984d687668cb
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-05-30 00:35:35 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -148,8 +148,8 @@ const About = () => {
           >
             <motion.h2
               className="text-3xl sm:text-4xl lg:text-6xl font-bold text-red-900 mb-6 tracking-tight"
-{/*               variants={itemVariants} */}
             >
+{/*               variants={itemVariants} */}
               About Chandigarh University MUN 2025
             </motion.h2>
             <motion.div
